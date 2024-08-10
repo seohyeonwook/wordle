f1dc0ce84c34ce3de6a608c7a353545394e936a6
seohyeonwook <skut01541@gamail.com>
2024-08-10 23:00:14 +0900

    wordle만들기
    wordle 만들기

---
+++ main.py
@@ -0,0 +1,43 @@
+a = 2
+print(a) 
+# 변수선언
+
+b = 1
+
+if b == 1:
+    print(2)
+else:
+    print(5)
+    
+    
+배열 = [1,2,3,4,5]
+print(배열)
+# 배열
+
+class A_가수:
+    def 장르():
+        print("재즈")
+    def 이름():
+        print("아무개")
+        
+A_가수.장르()
+
+def sayHello():
+    print("안녕하세요!")
+sayHello()
+# 클래스
+
+if True :
+    print("참입니다!")
+    
+if 1 > 0:
+    print("참입니다!")
+    
+for i in range(1,5):
+    print(i)
+# 조건문
+
+
+
+
+
